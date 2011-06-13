acf02794fe37a771b1fabe6a321a68fbdecc4997
Alex Neundorf <neundorf@kde.org>
2011-06-13 22:38:32 +0200

    make the packaging work again with the new directory layout
    Alex

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -8,33 +8,46 @@ include(CMakeParseArguments)
 
 set(SB_GIT_TAG "master" CACHE STRING "The git tag to use for cloning")
 
+set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+             PROPERTY EP_BASE ${CMAKE_CURRENT_BINARY_DIR}
+            )
+
 macro(sb_add_project _name )
-  option(BUILD_${_name} "Build subproject ${_name}" TRUE)
 
-  set(oneValueArgs CVS_REPOSITORY GIT_REPOSITORY SVN_REPOSITORY SOURCE_DIR )
-  set(multiValueArgs DEPENDS)
-  cmake_parse_arguments(_SB "" "${oneValueArgs}" "${multiValueArgs}"  ${ARGN})
-
-  if(EXISTS ${CMAKE_SOURCE_DIR}/${_name}/src/ ) # we are building an installed version of the source package
-    set(GET_SOURCES_ARGS SOURCE_DIR ${CMAKE_SOURCE_DIR}/${_name}/src/${_name}
-                         DOWNLOAD_COMMAND "")
+  if(EXISTS ${CMAKE_SOURCE_DIR}/ThisIsASourcePackage.valid ) # we are building an installed version of the source package
+    set(buildFromSourcePackage TRUE)
   else()
-    set(GET_SOURCES_ARGS DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/src/${_name}/ )
+    set(buildFromSourcePackage FALSE)
+  endif()
 
-    if(_SB_CVS_REPOSITORY)
-      set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} CVS_REPOSITORY ${_SB_CVS_REPOSITORY} )
-    elseif(_SB_GIT_REPOSITORY)
-      set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} GIT_REPOSITORY ${_SB_GIT_REPOSITORY} GIT_TAG ${SB_GIT_TAG} SOURCE_DIR ${CMAKE_BINARY_DIR}/src/${_name})
-    elseif(_SB_SVN_REPOSITORY)
-      set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} SVN_REPOSITORY ${_SB_SVN_REPOSITORY} )
-    elseif(_SB_SOURCE_DIR)
-      set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} SOURCE_DIR ${_SB_SOURCE_DIR} )
-    endif()
-#    set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} SOURCE_DIR ${CMAKE_BINARY_DIR}/src/${_name} )
+  if(EXISTS ${CMAKE_SOURCE_DIR}/${_name}  OR NOT buildFromSourcePackage)
+    option(BUILD_${_name} "Build subproject ${_name}" TRUE)
   endif()
 
   if (BUILD_${_name})
     message(STATUS "Adding project ${_name}")
+
+    set(oneValueArgs CVS_REPOSITORY GIT_REPOSITORY SVN_REPOSITORY SOURCE_DIR )
+    set(multiValueArgs DEPENDS)
+    cmake_parse_arguments(_SB "" "${oneValueArgs}" "${multiValueArgs}"  ${ARGN})
+
+    if(buildFromSourcePackage) # we are building an installed version of the source package
+      set(GET_SOURCES_ARGS SOURCE_DIR ${CMAKE_SOURCE_DIR}/${_name}
+                           DOWNLOAD_COMMAND "")
+    else()
+      set(GET_SOURCES_ARGS ) #DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/src/${_name}/ )
+
+      if(_SB_CVS_REPOSITORY)
+        set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} CVS_REPOSITORY ${_SB_CVS_REPOSITORY} )
+      elseif(_SB_GIT_REPOSITORY)
+        set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} GIT_REPOSITORY ${_SB_GIT_REPOSITORY} GIT_TAG ${SB_GIT_TAG} )
+      elseif(_SB_SVN_REPOSITORY)
+        set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} SVN_REPOSITORY ${_SB_SVN_REPOSITORY} )
+      elseif(_SB_SOURCE_DIR)
+        set(GET_SOURCES_ARGS ${GET_SOURCES_ARGS} SOURCE_DIR ${_SB_SOURCE_DIR} )
+      endif()
+    endif()
+
 
     set(DEPENDS_ARGS)
     if(_SB_DEPENDS)
@@ -57,11 +70,10 @@ macro(sb_add_project _name )
 
     externalproject_add(${_name}
                         ${_SB_UNPARSED_ARGUMENTS}
-#                        PREFIX ${_name}
                         ${GET_SOURCES_ARGS}
-                        TMP_DIR ${CMAKE_BINARY_DIR}/tmpfiles/${_name}
-                        STAMP_DIR ${CMAKE_BINARY_DIR}/stampfiles/${_name}
-                        BINARY_DIR ${CMAKE_BINARY_DIR}/build/${_name}
+#                        TMP_DIR ${CMAKE_BINARY_DIR}/tmpfiles/${_name}
+#                        STAMP_DIR ${CMAKE_BINARY_DIR}/stampfiles/${_name}
+#                        BINARY_DIR ${CMAKE_BINARY_DIR}/build/${_name}
                         INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
 #                        INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} -C${CMAKE_BINARY_DIR}/${_name}/build install DESTDIR=${CMAKE_BINARY_DIR}/Install
                         CMAKE_ARGS -DQT_QMAKE_EXECUTABLE=${QT_QMAKE_EXECUTABLE} -DCMAKE_PREFIX_PATH=${CMAKE_INSTALL_PREFIX} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
@@ -74,15 +86,25 @@ macro(sb_add_project _name )
 #                             DEPENDEES build)
 #
 #    externalProject_Add_StepTargets(${_name} package)
-    install(DIRECTORY ${CMAKE_BINARY_DIR}/${_name}/src/ DESTINATION src/${_name}/src/ )
+    install(DIRECTORY ${CMAKE_BINARY_DIR}/Source/${_name} DESTINATION Source )
 
     add_dependencies(UpdateAll ${_name}-update )
 #    add_dependencies(PackageAll ${_name}-package )
   else()
     message(STATUS "Skipping ${_name}")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/Build/${_name}
+                    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/Source/${_name}
+                    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/Download/${_name}
+                    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/Stamp/${_name}
+                    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/tmp/${_name}
+                    OUTPUT_QUIET ERROR_QUIET )
   endif()
 endmacro(sb_add_project)
 
-install(FILES CMakeLists.txt DESTINATION src )
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/ThisIsASourcePackage "This is a generated source package.")
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/ThisIsASourcePackage DESTINATION Source RENAME ThisIsASourcePackage.valid )
+install(FILES CMakeLists.txt DESTINATION Source )
+install(FILES ${CMAKE_CURRENT_LIST_FILE} DESTINATION . )
 
 set(CMAKE_SKIP_INSTALL_ALL_DEPENDENCY TRUE)
